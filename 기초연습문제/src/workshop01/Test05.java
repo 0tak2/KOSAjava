934cc6a269729c5ff76ca07b358c6538e7160fba
0tak <0tak2.code@gmail.com>
2022-12-15 18:51:01 +0900

    연습문제풀이

---
+++ 기초연습문제/src/workshop01/Test05.java
@@ -0,0 +1,11 @@
+package workshop01;
+
+public class Test05 {
+
+	public static void main(String[] args) {
+		int fahrenheit = 100;
+		float celcius = 5.0f/9.0f * (fahrenheit - 32.0f);
+		System.out.println("화씨: " + fahrenheit);
+		System.out.println("섭씨: " + celcius);
+	}
+}
