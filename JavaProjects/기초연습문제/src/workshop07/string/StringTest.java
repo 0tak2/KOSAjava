2449b0a9282a95993e3e5619494b10bbd761b620
0tak <0tak2.code@gmail.com>
2022-12-22 17:56:22 +0900

    SQL 연습문제 풀이

--- JavaProjects/기초연습문제/src/workshop07/string/StringTest.java
+++
@@ -1,8 +0,0 @@
-package workshop07.string;
-
-public class StringTest {
-	public static void main(String[] args) {
-		ConvertString converter = new ConvertString();
-		System.out.println(converter.convert(args[0]));
-	}
-}
