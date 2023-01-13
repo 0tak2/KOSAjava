a263fe1eefdc1cc4b40ace18afc2d88f9a90dd48
0tak <0tak2.code@gmail.com>
2023-01-13 13:27:45 +0900

    1월 13일 중간 커밋

---
+++ Projects-With-JS/myApp/2023-01-13/1_literal.js
@@ -0,0 +1,15 @@
+// 문자열 리터럴
+let tmp = '소리없는 아우성!';
+tmp = "소리없는 아우성!";
+tmp = `소리없는 아우성!`;
+
+// 객체 리터럴
+tmp = {
+    myName: '홍길동',
+    myAddr: '서울'
+};
+
+// 함수 리터럴
+tmp = function myFunc() {
+    console.log("이건 몰랐징");
+};
---
+++ Projects-With-JS/myApp/2023-01-13/3_object.js
@@ -0,0 +1,43 @@
+// ----------------------------------
+let myObj = {
+    'name': '홍길동', // Property 1
+    'age': 20, // Property 2
+    'addr': '서울', // Property 3
+    'myInfo': function() {
+        console.log('소리 없는 아우성!');
+    } // Property 4: 익명 함수. 이름 대신 키를 지정한 일반적인 형태
+};
+
+console.log(myObj.name); // 홍길동
+
+console.log(myObj.myInfo); // [Function: myInfo]
+myObj.myInfo(); // '소리 없는 아우성!'
+console.log();
+
+// ----------------------------------
+let anotherObj = {
+    'name': '홍길동'
+}
+
+console.log(anotherObj);
+// { name: '홍길동' } -> 키는 기본 문자열 처리.
+// Single Quote로 묶지 않아도 됨.
+/*
+let anotherObj = {
+    name: '홍길동'
+}
+*/
+
+// ----------------------------------
+let anotherObj2 = {
+    name: '홍길동',
+    10: 200
+}
+console.log(anotherObj2);
+// { '10': 200, name: '홍길동' }
+// 키는 문자열이기 떄문에, 수를 키로 지정하더라도
+// 문자로 형변환되어 처리됨
+
+// console.log(anotherObj2.10);
+// console.log(anotherObj2.'10'); // . 연산자로 접근할 수 없음
+console.log(anotherObj2['10']); // 대신 배열처럼 접근 가능. [연관배열] 인덱스로는 문자열을 정확히 넘겨야 함.
