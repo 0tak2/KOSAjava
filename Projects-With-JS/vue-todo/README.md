a7951811a2cffc3db52b24e55858d86d615083b2
0tak <0tak2.code@gmail.com>
2023-02-02 11:06:00 +0900

    new vue project set

---
+++ Projects-With-JS/vue-todo/README.md
@@ -0,0 +1,16 @@
+# vue-todo
+
+Vue 실습을 위한 간단한 TO-DO 어플리케이션입니다.
+
+## Build Setup
+
+``` bash
+# 의존 패키지 설치
+npm install
+
+# localhost:8080로 앱 Serve
+npm run dev
+
+# 앱 빌드
+npm run build
+```
---
+++ Projects-With-JS/vue-todo/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>이것은 소리없는 아우성!</h1>
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: '신사임당'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
