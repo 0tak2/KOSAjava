93f62e734c56d8f7a6c821c744d65b6f047cf4a0
0tak <0tak2.code@gmail.com>
2023-02-03 09:40:27 +0900

    텍스트 필드에 날짜 입력시 형식에 맞지 않으면 오류가 발생하던 이슈를 수정

--- Projects-With-JS/vue-boxoffice-sfc/src/components/BoxControl.vue
+++ Projects-With-JS/vue-boxoffice-sfc/src/components/BoxControl.vue
@@ -9,17 +9,20 @@
         </v-row>
 
         <v-row v-if="showDate" justify="center" class="date-control">
-            <v-date-picker v-model="pickedDate" v-on:click:date="handleClickDate"></v-date-picker>
+            <v-date-picker v-model="safeDate" v-on:click:date="handleClickDate"></v-date-picker>
         </v-row>
     </div>
 </template>
 
 <script>
+/* eslint-disable no-new */
+
 export default {
   data() {
     return {
       showDate: false,
       pickedDate: this.$route.params.date,
+      safeDate: this.$route.params.date,
     };
   },
   methods: {
@@ -37,6 +40,37 @@ export default {
     handleSearchBtn() {
       this.$emit('request', this.pickedDate);
       this.handleClickDate();
+    },
+  },
+  watch: {
+    pickedDate(val) {
+      let inputDate = val;
+      if (inputDate.search(/2\d\d\d-[0-1]*\d-[0-3]*\d/g) > -1) { // 날짜 유효성 검사
+        const year = inputDate.split('-')[0];
+        let month = inputDate.split('-')[1];
+        let day = inputDate.split('-')[2];
+
+        if (Number(month) < 1 || Number(month) > 12) {
+          return;
+        }
+        if (Number(day) < 1 || Number(day) > 31) {
+          return;
+        }
+
+        if (month.length === 1) { // 자릿수 확인
+          month = `0${month}`;
+          inputDate = `${year}-${month}-${day}`;
+        }
+        if (day.length === 1) {
+          day = `0${day}`;
+          inputDate = `${year}-${month}-${day}`;
+        }
+
+        this.safeDate = inputDate; // 검사 성공시 날짜 컨트롤에 바인딩
+      }
+    },
+    safeDate(val) {
+      this.pickedDate = val;
     },
   },
 };
