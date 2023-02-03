6689ffe2dc4d82342411c384a0988eb677a2e537
0tak <0tak2.code@gmail.com>
2023-02-03 10:14:33 +0900

    날짜 유효성 검사 보완

--- Projects-With-JS/vue-boxoffice-sfc/src/components/BoxControl.vue
+++ Projects-With-JS/vue-boxoffice-sfc/src/components/BoxControl.vue
@@ -1,17 +1,33 @@
 <template>
-    <div>
-        <v-row justify="center" class="main-control">
-            <v-text-field label="조회일자"
-                        v-on:click="toggleDate"
-                        v-on:keydown="onKeydown"
-                        v-model="pickedDate"></v-text-field>
-            <v-btn elevation="2" class="search-btn" v-on:click="handleSearchBtn">조회</v-btn>
-        </v-row>
+  <div>
+    <v-alert
+      v-model="alert"
+      close-text="Close Alert"
+      dense
+      type="warning"
+      dismissible
+    >
+      잘못된 날짜를 입력하셨습니다. 다시 한 번 확인해주세요.
+    </v-alert>
+    <v-row justify="center" class="main-control">
+      <v-text-field
+        label="조회일자"
+        v-on:click="toggleDate"
+        v-on:keyup.enter="requestSearch"
+        v-model="pickedDate"
+      ></v-text-field>
+      <v-btn elevation="2" class="search-btn" v-on:click="requestSearch"
+        >조회</v-btn
+      >
+    </v-row>
 
-        <v-row v-if="showDate" justify="center" class="date-control">
-            <v-date-picker v-model="safeDate" v-on:click:date="handleClickDate"></v-date-picker>
-        </v-row>
-    </div>
+    <v-row v-if="showDate" justify="center" class="date-control">
+      <v-date-picker
+        v-model="safeDate"
+        v-on:click:date="closeDateControl"
+      ></v-date-picker>
+    </v-row>
+  </div>
 </template>
 
 <script>
@@ -23,29 +39,34 @@ export default {
       showDate: false,
       pickedDate: this.$route.params.date,
       safeDate: this.$route.params.date,
+      alert: false,
     };
   },
   methods: {
     toggleDate() {
       this.showDate = !this.showDate;
+      this.alert = false;
     },
-    onKeydown(keyboardEvent) {
-      if (keyboardEvent.key === 'Enter') {
-        this.handleSearchBtn();
-      }
-    },
-    handleClickDate() {
+    closeDateControl() {
       this.showDate = false;
     },
-    handleSearchBtn() {
+    requestSearch() {
+      if (this.pickedDate !== this.safeDate) {
+        this.alert = true;
+        this.closeDateControl();
+        return;
+      }
       this.$emit('request', this.pickedDate);
-      this.handleClickDate();
+      this.closeDateControl();
+      this.alert = false;
     },
   },
   watch: {
     pickedDate(val) {
       let inputDate = val;
-      if (inputDate.search(/2\d\d\d-[0-1]*\d-[0-3]*\d/g) > -1) { // 날짜 유효성 검사
+      const matched = inputDate.match(/2\d\d\d-[0-1]?\d-[0-3]?\d/g);
+      if (matched !== null && matched[0] === val) {
+        // 날짜 유효성 검사
         const year = inputDate.split('-')[0];
         let month = inputDate.split('-')[1];
         let day = inputDate.split('-')[2];
@@ -57,7 +78,8 @@ export default {
           return;
         }
 
-        if (month.length === 1) { // 자릿수 확인
+        if (month.length === 1) {
+          // 자릿수 확인
           month = `0${month}`;
           inputDate = `${year}-${month}-${day}`;
         }
@@ -78,18 +100,18 @@ export default {
 
 <style scoped>
 .date-control {
-    position: absolute;
-    z-index: 8;
-    top: 85px;
+  position: absolute;
+  z-index: 8;
+  top: 85px;
 }
 
 .main-control {
-    margin-top: 30px;
-    margin-bottom: 30px;
+  margin-top: 30px;
+  margin-bottom: 30px;
 }
 
 .search-btn {
-    margin-top: 10px;
-    margin-left: 10px;
+  margin-top: 10px;
+  margin-left: 10px;
 }
 </style>
