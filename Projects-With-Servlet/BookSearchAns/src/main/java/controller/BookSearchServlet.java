aaa69bab83f8eaa02555a3e646f06f6c7bc174f1
0tak <0tak2.code@gmail.com>
2023-02-08 18:48:30 +0900

    교수님과 같이 진행한 책 검색 예제

--- Projects-With-Servlet/BookSearchAns/src/main/java/controller/BookSearchServlet.java
+++ Projects-With-Servlet/BookSearchAns/src/main/java/controller/BookSearchServlet.java
@@ -11,11 +11,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 
-import dao.BookDAO;
 import mybatis.MyBatisConnectionFactory;
-import vo.BookVO;
+import vo.Book;
 
 /**
  * Servlet implementation class BookSearchServlet
@@ -31,7 +29,7 @@ public class BookSearchServlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-    
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -44,22 +42,22 @@ public class BookSearchServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 1. 입력 처리
-		request.setCharacterEncoding("UTF-8");
-		String keyword = request.getParameter("keyword");
-		String priceStr = request.getParameter("price");
 		
-		int price = 0;
-		if (priceStr != null) {
-			price = Integer.parseInt(priceStr);
-		}
+		// 1. 입력 처리
+		request.setCharacterEncoding("UTF-8"); // 한글
+		
+		String keyword = request.getParameter("keyword");
+		String price = request.getParameter("price");
 		
 		// 2. 로직 처리
-		BookVO target = new BookVO(null, keyword, null, price);
-		SqlSessionFactory factory = MyBatisConnectionFactory.getSqlSessionFactory();
-		SqlSession session = factory.openSession();
-		BookDAO dao = new BookDAO(session);
-		List<BookVO> list = dao.select(target);
+		Book book = new Book(); // MyBatis에 넘겨줄 VO 객체
+		book.setBtitle(keyword);
+		book.setBprice(Integer.parseInt(price));
+		
+		SqlSession session =
+				MyBatisConnectionFactory.getSqlSessionFactory().openSession();
+		
+		List<Book> result = session.selectList("myBook.selectBookByKeyword", book);
 		session.close();
 		
 		// 3. 출력 처리
@@ -68,22 +66,19 @@ public class BookSearchServlet extends HttpServlet {
 		out.println("<html>");
 		out.println("<head></head>");
 		out.println("<body>");
-		out.println("<table>");
-		out.println("<thead><tr><th>ISBN</th><th>제목</th><th>저자</th><th>가격</th></tr></thead>");
-		out.println("<tbody>");
 		out.println("<h1>조회 결과</h1>");
-		out.printf("<span>조건: 키워드=%s, 가격<%d원</span><br>\n", keyword, price);
-		out.println("<a href='bookSearch.html'>조회 화면으로 돌아가기</a><br><br>");
-		out.printf("<span>총 %d건을 조회하였습니다.</span><br><br>\n", list.size());
-		for (BookVO book : list) {
-			out.printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",
-					book.getBisbn(), book.getBtitle(), book.getBauthor(), book.getBprice());
+		out.printf("<h3>컴색 키워드: %s</h3>", keyword, price);
+		out.printf("<h3>컴색 가격: %s</h3>", price);
+		out.println("<ul>");
+		for (Book item : result) {
+			out.printf("<li>%s, %s, %s, %d</li>",
+					item.getBisbn(), item.getBtitle(), item.getBauthor(), item.getBprice());
 		}
-		out.println("</tbody>");
-		out.println("</table>");	
+		out.println("</ul>");
 		out.println("</body>");
 		out.println("</html>");
 		out.close();
+		
 	}
 
 }
---
+++ Projects-With-Servlet/BookSearchAns/src/main/java/vo/Book.java
@@ -0,0 +1,53 @@
+package vo;
+
+public class Book {
+
+	private String bisbn;
+	private String btitle;
+	private String bauthor;
+	private int bprice;
+	
+	public Book() {
+	}
+
+	public Book(String bisbn, String btitle, String bauthor, int bprice) {
+		super();
+		this.bisbn = bisbn;
+		this.btitle = btitle;
+		this.bauthor = bauthor;
+		this.bprice = bprice;
+	}
+
+	public String getBisbn() {
+		return bisbn;
+	}
+
+	public void setBisbn(String bisbn) {
+		this.bisbn = bisbn;
+	}
+
+	public String getBtitle() {
+		return btitle;
+	}
+
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+
+	public String getBauthor() {
+		return bauthor;
+	}
+
+	public void setBauthor(String bauthor) {
+		this.bauthor = bauthor;
+	}
+
+	public int getBprice() {
+		return bprice;
+	}
+
+	public void setBprice(int bprice) {
+		this.bprice = bprice;
+	}
+	
+}
