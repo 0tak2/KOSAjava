0dcdaa05e2ff36e26ffd4caafc5ecffb1ef614b8
0tak <0tak2.code@gmail.com>
2023-02-08 18:48:30 +0900

    교수님과 같이 진행한 책 검색 예제

---
+++ Projects-With-Servlet/BookSearchAns/src/main/java/mybatis/MyBatisConnectionFactory.java
@@ -0,0 +1,31 @@
+package mybatis;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MyBatisConnectionFactory {
+   private static SqlSessionFactory sqlSessionFactory;
+   
+   static {
+      
+      String resource = "./SqlMapConfig.xml"; 
+      try {
+         Reader reader = Resources.getResourceAsReader(resource);
+         
+         if( sqlSessionFactory == null ) {
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+         }
+      } catch (IOException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+   }
+   
+   public static SqlSessionFactory getSqlSessionFactory() {
+      return sqlSessionFactory;
+   }
+}
