963eac6f24acb82187c28bc0bf67f5a4e1d02f58
0tak2 <0tak2.code@gmail.com>
2023-02-10 17:53:13 +0900

    테스트용 SQL

---
+++ Projects-With-Servlet/BoardProject0209/base.sql
@@ -0,0 +1,55 @@
+-- 스키마 만들기
+DROP DATABASE IF EXISTS simpleboard;
+CREATE DATABASE simpleboard;
+
+USE simpleboard;
+
+-- 멤버 테이블 만들기
+CREATE TABLE members (
+	memberId  VARCHAR(10)  NOT NULL PRIMARY KEY,
+	memberName  VARCHAR(20)  NOT NULL,
+	memberPw  VARCHAR(20) NOT NULL
+);
+
+-- 멤버 테이블 테스트 데이터
+INSERT INTO members VALUES('hong', '홍길동', '1234');
+INSERT INTO members VALUES('lim', '임영택', '1234');
+
+-- 게시글 테이블 만들기
+CREATE TABLE articles (
+	articleNum  INTEGER  NOT NULL PRIMARY KEY auto_increment,
+	articleTitle  VARCHAR(30)  NOT NULL,
+	articleContent  VARCHAR(256)  NOT NULL,
+	articleAuthor  VARCHAR(10),
+	articleDate  DATETIME  NOT NULL DEFAULT NOW(),
+	articleLike  INTEGER,
+	FOREIGN KEY (articleAuthor) REFERENCES members(memberId)
+);
+
+-- 게시글 테이블 테스트 데이터
+INSERT INTO articles(articleTitle, articleAuthor, articleContent, articleLike)
+		VALUES('플스 팔아요', 'hong', '싸게 팝니다. 네고 사절', 5);
+
+INSERT INTO articles(articleTitle, articleAuthor, articleContent, articleLike)
+		VALUES('갤럭시 팔아요', 'hong', '5만원에 팝니다. 네고 사절', 2);
+
+-- 댓글 테이블 만들기
+CREATE TABLE comments (
+	commentNum  INTEGER  NOT NULL PRIMARY KEY auto_increment,
+	commentContent  VARCHAR(256)  NOT NULL,
+	commentAuthor  VARCHAR(10),
+	commentDate  DATETIME  NOT NULL DEFAULT NOW(),
+	commentArticle INTEGER,
+	FOREIGN KEY (commentAuthor) REFERENCES members(memberId),
+	FOREIGN KEY (commentArticle) REFERENCES articles(articleNum) ON DELETE CASCADE
+);
+
+-- 댓글 테이블 테스트 데이터
+INSERT INTO comments(commentContent, commentAuthor, commentArticle)
+		VALUES('판매 완료되었습니다.', 'hong', 1);
+
+INSERT INTO comments(commentContent, commentAuthor, commentArticle)
+		VALUES('가격 인하 - 3만원... 제발 사주세요', 'hong', 2);
+
+
+
