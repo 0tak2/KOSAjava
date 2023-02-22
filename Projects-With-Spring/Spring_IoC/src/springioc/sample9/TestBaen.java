51c06781cbea43f58ecc20101241306f39923503
0tak2 <0tak2.code@gmail.com>
2023-02-22 12:29:13 +0900

    2월 22일 중간 커밋

---
+++ Projects-With-Spring/Spring_IoC/src/springioc/sample9/TestBaen.java
@@ -0,0 +1,29 @@
+package springioc.sample9;
+
+public class TestBaen {
+
+	private DataBean data1;
+	private DataBean data2;
+	
+	public TestBaen() {
+		System.out.println("[TestBean] 기본 생성자 호출");
+	}
+
+	public DataBean getData1() {
+		return data1;
+	}
+
+	public void setData1(DataBean data1) {
+		this.data1 = data1;
+		System.out.println("[TestBean] setData1 메서드 호출");
+	}
+
+	public DataBean getData2() {
+		return data2;
+	}
+
+	public void setData2(DataBean data2) {
+		this.data2 = data2;
+		System.out.println("[TestBean] setData2 메서드 호출");
+	}
+}
